d273652b2d40399420ed205ab67598336cd9bce9
Adam Porter <adam@alphapapa.net>
2016-10-07 03:33:44 -0500

    Use Python 3, pathlib, etc.
    pathlib is *really* nice.

--- pytrash.py
+++ pytrash.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # * Imports
 
-import ConfigParser
 import logging as log
-import os
 import re
 
+from configparser import ConfigParser, ParsingError, NoSectionError, NoOptionError
 from datetime import datetime
-
+from pathlib import Path
 
 # * Constants
 
@@ -18,19 +17,20 @@ TRASHINFO_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 # * Classes
 
+class InvalidTrashBin(Exception):
+    pass
 
 class TrashBin(object):
     """Represents an XDG trash bin."""
 
     def __init__(self, path):
-        self.path = path
-        self.files_path = os.path.join(self.path, 'files')
-        self.info_path = os.path.join(self.path, 'info')
+        self.path = Path(path)
+        self.files_path = self.path / "files"
+        self.info_path = self.path / "info"
+        self.files = []
 
         # Verify path is a trash bin
-        if not self._verify():
-            log.critical("Path does not appear to be a valid XDG trash bin: %s", self.path)
-            return False
+        self._verify()
 
     def _verify(self):
         """Return True if TrashBin appears to be a valid XDG trash bin."""
@@ -38,56 +38,70 @@ class TrashBin(object):
         # TODO: Should we check write permission too?
 
         # Verify dirs exist
-        if not (os.path.exists(self.files_path) and
-                os.path.exists(self.info_path)):
-                return False
+        if not (self.files_path.is_dir() and
+                self.info_path.is_dir()):
+            raise InvalidTrashBin("Path does not appear to be a valid XDG trash bin: %s", self.path)
 
-    def item_exists(self, name):
-        """Return True if a file or directory _name_ exists in the trash bin.
+    # def item_exists(self, name):
+    #     """Return True if a file or directory _name_ exists in the trash bin.
 
-        Checks both "Trash/files/_name_" and "Trash/info/_name_.trashinfo" paths.
-        """
+    #     Checks both "Trash/files/_name_" and "Trash/info/_name_.trashinfo" paths.
+    #     """
 
-        for test_path in [os.path.join(self.files_path, name),
-                          os.path.join(self.info_path, "%s.trashinfo" % name)]:
-            if os.path.exists(test_path):
-                return True
+    #     if all(
+    #     for test_path in [self.files_path / name,
+    #                       self.info_path / "%s.trashinfo".format(name)]:
+    #         if (test_path):
+    #             return True
 
-        return False
+    #     return False
 
+    def list_items(self):
+        """List items in trash bin.
+
+        Trashed directories are not descended into."""
+
+        if not self.files:
+            self._read_info_files()
+
+        print(self.files)
+
+    def _read_info_files(self):
+        """Read .trashinfo files in bin and populate list of files."""
+
+        for f in self.info_path.glob("*.trashinfo"):
+            try:
+                self.files.append(TrashedPath(info_file=f))
+            except Exception:
+                raise Exception("sigh")
+            else:
+                log.debug("Read info file: %s", f)
+
+        log.debug("Read %s info files", len(self.files))
 
 class TrashedPath(object):
     """Represents a trashed (or to-be-trashed) file or directory."""
 
-    def __init__(self, path=None, trashinfo_file_path=None):
+    def __init__(self, bin=None, path=None, info_file=None):
         # TODO: Should I pass this in as an argument instead?
-        global trash_bin
-        self.trash_bin = trash_bin
+        self.bin = bin
 
         self.original_path = None
         self.trashed = False
-
-        # Name in trash bin (possibly with suffix for uniqueness)
-        self.basename = None
 
         # TODO: Check FreeDesktop.org specs to see if timezone/UTC is specified.
         self.date_trashed = None  # datetime object in UTC
 
         if path:
-            self.original_path = path
-            self.basename = self._basename(path)
-        if trashinfo_file_path:
-            self.trashinfo_file_path = trashinfo_file_path
+            self.original_path = Path(path)
+        if info_file:
+            if isinstance(info_file, str):
+                self.info_file = Path(info_file)
+            elif isinstance(info_file, Path):
+                self.info_file = info_file
+
             self._read_trashinfo_file()
-
-    def _basename(self, path):
-        """Return basename of _path_.
-
-        A wrapper for os.path.basename() that strips the slash off the
-        end, making it behave like the "basename" command.
-        """
-
-        return os.path.basename(path.rstrip('/'))
+            self.trashed_path = self.info_file.parent.parent / "files" / self.info_file.stem
 
     def _change_basename_if_necessary(self):
         """Rename self.basename if it already exists in the trash bin."""
@@ -98,7 +112,7 @@ class TrashedPath(object):
         # conflicts.
 
         tries = 0
-        while self.trash_bin.item_exists(self.basename):
+        while self.bin.item_exists(self.basename):
             log.debug("Path exists in trash: ", self.basename)
 
             if tries == 100:
@@ -124,68 +138,42 @@ class TrashedPath(object):
     def _read_trashinfo_file(self):
         """Read .trashinfo file and set object properties."""
 
-        trashinfo = ConfigParser.SafeConfigParser(allow_no_value=True)
+        trashinfo = ConfigParser()
 
         # Read file
         try:
-            trashinfo.read(self.trashinfo_file_path)
-        except:
-            log.critical("Unable to read trashinfo file: %s", self.trashinfo_file_path)
+            trashinfo.read(str(self.info_file))
+            self.original_path = trashinfo.get(TRASHINFO_SECTION_HEADER, 'path')
+            self.date_trashed = datetime.strptime(trashinfo.get(TRASHINFO_SECTION_HEADER, 'deletiondate'), TRASHINFO_DATE_FORMAT)
+        except (ParsingError, NoSectionError, NoOptionError) as e:
+            log.warning("trashinfo file appears invalid or empty: %s, %s", e.message, self.info_file)
             raise
 
-        # Set basename from trashinfo_file_path
-        self.basename = os.path.basename(self.trashinfo_file_path)
-
-        # Load section
-        if trashinfo.has_section(TRASHINFO_SECTION_HEADER):
-            data = trashinfo.items(TRASHINFO_SECTION_HEADER)
-        else:
-            log.critical("trashinfo file appears invalid or empty: %s", self.trashinfo_file_path)
-            raise
-
-        # Read and assign attributes
-        if 'Path' in data:
-            self.original_path = data['Path']
-        else:
-            # The Path is missing, so the trashinfo file is useless
-            log.critical("trashinfo file has no Path attribute: %s", self.trashinfo_file_path)
-            raise
-
-        if 'DeletionDate' in data:
-            try:
-                self.date_trashed = datetime.strptime(data['DeletionDate'], TRASHINFO_DATE_FORMAT)
-            except ValueError:
-                log.critical("Unable to parse date (%s) from trashinfo file: %s", data['DeletionDate'], self.trashinfo_file_path)
-                raise
-        else:
-            log.warning("trashinfo file has no DeletionDate attribute: %s", self.trashinfo_file_path)
+        self.trashed = True
 
     def _write_trashinfo_file(self):
         """Write .trashinfo file for trashed path."""
 
         # Make trashinfo file path if it doesn't exist
-        if not self.trashinfo_file_path:
-            self.trashinfo_file_path = os.path.join(self.trash_bin.info_path, "%s.trashinfo" % self.basename)
-            log.debug("Set trashinfo file path: ", self.trashinfo_file_path)
+        if not self.info_file:
+            self.info_file = self.bin.info_path / "%s.trashinfo".format(self.path.name)
 
         # Verify trashinfo file doesn't exist
-        if os.path.exists(self.trashinfo_file_path):
-            log.warning("Trashinfo file already exists: ", self.trashinfo_file_path)
-            return False
+        if self.info_file.exists:
+            raise Exception("Trashinfo file already exists: ", self.info_file)
 
         # Setup trashinfo
-        trashinfo = ConfigParser.SafeConfigParser()
+        trashinfo = SafeConfigParser()
         trashinfo.add_section(TRASHINFO_SECTION_HEADER)
         trashinfo.set(TRASHINFO_SECTION_HEADER, 'Path', self.path)
         trashinfo.set(TRASHINFO_SECTION_HEADER, 'DeletionDate', TRASHINFO_DATE_FORMAT % self.date_trashed)
 
         # Write the file
         try:
-            with open(os.path.join(self.trash_bin.info_path, self.filename), 'wb') as f:
+            with open(self.info_file, 'wb') as f:
                 trashinfo.write(f)
         except:
-            log.critical("Unable to write trashinfo file: %s", self.trashinfo_file_path)
-            raise
+            raise Exception("Unable to write trashinfo file: %s", self.info_file)
 
     def restore(self, dest_path=None):
         """Restore a path from the trash to its original location.  If
@@ -228,4 +216,5 @@ class TrashedPath(object):
         # course, there's still a chance of a race condition, but if
         # os.path.rename overwrites, is it possible to avoid this?
 
-trash_bin = TrashBin()
+if __name__ == "__main__":
+    trash_bin = TrashBin()
